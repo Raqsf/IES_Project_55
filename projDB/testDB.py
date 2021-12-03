32af94b378e9528a7a9d40e40d618620c7554a89
TheScorpoi <sobral@ua.pt>
2021-12-03 23:55:05 +0000

    add python file to try cnnection to db

---
+++ projDB/testDB.py
@@ -0,0 +1,27 @@
+import sys
+import mysql.connector
+from mysql.connector import Error
+
+# Usage: $ python testMysql.py 'root' 'password'
+
+
+def connect():
+    """ Connect to MySQL database """
+    conn = None
+    try:
+        conn = mysql.connector.connect(host='localhost',
+                                       database='vacinas',
+                                       password='root')
+        if conn.is_connected():
+            print('Connected to MySQL database')
+
+    except Error as e:
+        print(e)
+
+    finally:
+        if conn is not None and conn.is_connected():
+            conn.close()
+
+
+if __name__ == '__main__':
+    connect()
