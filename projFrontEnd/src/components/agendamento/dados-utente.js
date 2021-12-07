aea8f4dd2c662780d103e354561e3e5f757196e8
Raqsf <raquelsf@ua.pt>
2021-12-07 21:52:26 +0000

    User's view done

---
+++ projFrontEnd/src/components/agendamento/dados-utente.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import TextField from '@mui/material/TextField';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import Stack from '@mui/material/Stack';
+import DesktopDatePicker from '@mui/lab/DesktopDatePicker';
+import { FormControl,FormHelperText,Button, Container } from '@mui/material';
+import { Box } from "@mui/system";
+
+export default function DadosUtente() {
+  const [utente, setUtente] = useState("");
+  const [nome, setNome] = useState("");
+  const [date, setDate] = useState(new Date());
+
+  function validateForm() {
+    return utente.length > 0 && nome.length > 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <Container>
+      <Box className="DadosUtente" onSubmit={handleSubmit} sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          pt: 3
+        }}>
+        <FormControl variant="outlined">
+              <TextField fullWidth label="Número de Utente"
+                  value={utente}
+                  onChange={(e) => setUtente(e.target.value)}
+                  style={{ marginTop: "20px" }}/>
+              <FormHelperText id="user-number-helper-text">Campo Nº Utente Saúde no Cartão de Cidadão.</FormHelperText> 
+              <TextField fullWidth label="Nome Completo"
+                      value={nome}
+                      onChange={(e) => setNome(e.target.value)}
+                      style={{ marginTop: "20px" }}/>
+              <LocalizationProvider dateAdapter={AdapterDateFns}>
+                  <Stack spacing={2} style={{ marginTop: "20px" }}>
+                      <DesktopDatePicker
+                      label="Data de Nascimento"
+                      
+                      disableFuture
+                      value={date}
+                      minDate={new Date('1900-01-01')}
+                      onChange={(newValue) => {
+                          setDate(newValue);
+                      }}
+                      renderInput={(params) => <TextField {...params} />}
+                      />
+                  </Stack>
+              </LocalizationProvider>
+              <Button 
+                href="/success" 
+                variant="contained" 
+                size="lg" 
+                type="submit" 
+                disabled={!validateForm()}
+                style={{ marginTop: "20px" }}
+              >
+                  Validação
+              </Button>
+          </FormControl>
+      </Box>
+    </Container>
+  );
+}
---
+++ projFrontEnd/src/components/check_schedule/form.js
@@ -0,0 +1,57 @@
+import { FormControl, FormHelperText, TextField, Button } from '@mui/material';
+import React, { useState } from "react";
+
+export default function FormVaccinationInfo() {
+
+    const [utente, setUtente] = useState("");
+    const [nome, setNome] = useState("");
+
+    function validateForm() {
+        return utente.length > 0 && !isNaN(+utente) && nome.length > 0;
+    }
+
+    function handleSubmit(event) {
+        //alert("HELLO");
+        //event.preventDefault();
+    }
+
+    return (
+        <form
+        component="form" 
+        action="/vaccination_info"
+        onSubmit={(e) => {
+            handleSubmit(e);
+          }} >
+          <div className="row" style={{ marginTop: "20px" }}>
+              <FormControl variant="outlined">
+                  <TextField
+                      style={{ marginTop: "10px" }}
+                      id="user-number"
+                      label="Número de Utente"
+                      value={utente}
+                      onChange={(e) => setUtente(e.target.value)}
+                      variant="outlined"
+                  />
+                  <FormHelperText id="user-number-helper-text">Campo Nº Utente Saúde no Cartão de Cidadão.</FormHelperText> 
+                  <TextField
+                      style={{ marginTop: "20px" }}
+                      id="user-name"
+                      label="Nome completo"
+                      value={nome}
+                      onChange={(e) => setNome(e.target.value)}
+                      variant="outlined"
+                  />
+                  <Button 
+                    variant="contained"
+                    style={{ marginTop: "20px" }}
+                    type="submit"
+                    label="Submit"
+                    disabled={!validateForm()}
+                  >
+                    CONSULTAR
+                  </Button>
+              </FormControl>
+          </div>
+        </form>
+      );
+} 
