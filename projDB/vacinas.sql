cc87ad2256a2049f4355efd54f81a7af9e2ef9c1
Raqsf <raquelsf@ua.pt>
2021-12-19 14:37:46 +0000

    Database

--- projDB/vacinas.sql
+++ projDB/vacinas.sql
@@ -1,7 +1,7 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET TIME_ZONE = "+00:00";
 
-CREATE DATABASE IF NOT EXISTS `vaccinationdb` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+CREATE DATABASE IF NOT EXISTS `vaccinationdb` DEFAULT CHARACTER SET latin1;
 USE `vaccinationdb`;
 
 CREATE TABLE IF NOT EXISTS `Pessoa` (
@@ -14,24 +14,6 @@ CREATE TABLE IF NOT EXISTS `Pessoa` (
 
 	PRIMARY KEY(`n_utente`)
 );
-GO
-
-
-CREATE TABLE IF NOT EXISTS `Vacina` (
-    `n_vacina`			    INT		  AUTO_INCREMENT          NOT NULL,
-    'lote'		            INT         		NOT NULL,
-    `nome`		            VARCHAR(256)		NOT NULL,
-    `data_rececao`	        DATE            	NOT NULL,
-    `administrada_a`		INT,
-    `data_administracao`    DATE,
-    `centro_vacinacao`		INT        NOT NULL,
-
-    PRIMARY KEY(`n_vacina`),
-    FOREIGN KEY(`atribuida_a`) REFERENCES `Pessoa`(`n_utente`)
-    FOREIGN KEY(`administrada_a`) REFERENCES `Pessoa`(`n_utente`)
-    FOREIGN KEY(`centro_vacinacao`) REFERENCES `Centro_vacionacao`(`id`)
-);
-GO
 
 CREATE TABLE IF NOT EXISTS `Centro_vacinacao` (
     `id`			                INT		  AUTO_INCREMENT          NOT NULL,
@@ -43,11 +25,19 @@ CREATE TABLE IF NOT EXISTS `Centro_vacinacao` (
     PRIMARY KEY(`id`)
 );
 
-INSERT INTO `Centro_vacinacao` (`id`, `nome`, `morada`, `capacidade_max`, `capacodade_atual`) VALUES 
-(1, 'Centro de Vacinação do Porto', 'Porto', 15, 0),
-(2, 'Centro de Vacinação do Lisboa', 'Lisboa', 23, 0),
-(3, 'Centro de Vacinação do Coimbra', 'Coimbra', 5, 0),
-(4, 'Centro de Vacinação do Aveiro', 'Aveiro', 8, 0);
+CREATE TABLE IF NOT EXISTS `Vacina` (
+    `n_vacina`			    INT		  AUTO_INCREMENT          NOT NULL,
+    `lote`		            INT         		NOT NULL,
+    `nome`		            VARCHAR(256)		NOT NULL,
+    `data_rececao`	        DATE            	NOT NULL,
+    `administrada_a`		INT                 NOT NULL,
+    `data_administracao`    DATE,
+    `centro_vacinacao`		INT        NOT NULL,
+
+    PRIMARY KEY(`n_vacina`),
+    FOREIGN KEY(`administrada_a`) REFERENCES `Pessoa`(`n_utente`),
+    FOREIGN KEY(`centro_vacinacao`) REFERENCES `Centro_vacinacao`(`id`)
+);
 
 CREATE TABLE IF NOT EXISTS `Lote` (
     `id`			                INT		  AUTO_INCREMENT          NOT NULL,
@@ -55,6 +45,12 @@ CREATE TABLE IF NOT EXISTS `Lote` (
     `data_validade`		            DATE            	NOT NULL,
     `atribuida_ao_centro`		    INT            	    NOT NULL,
 
-    PRIMARY KEY(`id`)
+    PRIMARY KEY(`id`),
     FOREIGN KEY (`atribuida_ao_centro`) REFERENCES `Centro_vacinacao`(`id`)
 );
+
+INSERT INTO `Centro_vacinacao` (`id`, `nome`, `morada`, `capacidade_max`, `capacidade_atual`) VALUES 
+(1, 'Centro de Vacinação do Porto', 'Porto', 15, 0),
+(2, 'Centro de Vacinação do Lisboa', 'Lisboa', 23, 0),
+(3, 'Centro de Vacinação do Coimbra', 'Coimbra', 5, 0),
+(4, 'Centro de Vacinação do Aveiro', 'Aveiro', 8, 0);
