998fec837784e582bf3121290e4a55aea881ee98
TheScorpoi <sobral@ua.pt>
2021-12-19 19:13:03 +0000

    update consumer

--- projService/vaccinationdeskservice/src/main/java/com/vaccinationdesk/vaccinationdeskservice/broker/MQConsumer.java
+++ projService/vaccinationdeskservice/src/main/java/com/vaccinationdesk/vaccinationdeskservice/broker/MQConsumer.java
@@ -17,6 +17,7 @@ public class MQConsumer {
     @RabbitListener(queues = MQConfig.QUEUE)
     public void listen(String input) {
         // TODO: Na parte de ter JSONObjects dentro de outros tentar fazer com um iterator (map, i guess)
+        //TODO e fazer isto com as classes, já que estao criadas, pode ser um mapa de Utente for instance
         JSONObject json = new JSONObject(input);
         String messageType = json.getString("type");
         switch (messageType) {
@@ -30,26 +31,27 @@ public class MQConsumer {
                 int quantity = json.getInt("quantity");
                 quantityList.clear();
                 quantityList.add(quantity);
-                System.out.println(quantityList);
                 break;
             default:
-                break;
+                break;                
         }
     }
 
     public void addScheduleVaccines(JSONObject json) {
         int n_utente = json.getJSONObject("utente").getInt("n_utente");
         String nome = json.getJSONObject("utente").getString("nome");
-        String data_vacina = json.getJSONObject("utente").getString("data_vacina");
+        String email = json.getJSONObject("utente").getString("email");
         String data_nasc = json.getJSONObject("utente").getString("data_nasc");
         String local = json.getJSONObject("utente").getString("local");
         String doencas = json.getJSONObject("utente").getString("doença");
+        String data_vacina = json.getJSONObject("utente").getString("data_vacina");
         List<String> utente_list = new ArrayList<String>();
         utente_list.add(nome);
-        utente_list.add(data_vacina);
+        utente_list.add(email);
         utente_list.add(data_nasc);
         utente_list.add(local);
         utente_list.add(doencas);
+        utente_list.add(data_vacina);
 
         scheduleVaccineMap.put(n_utente, utente_list.toString());
     }
@@ -57,16 +59,18 @@ public class MQConsumer {
     public void addGettingVaccine(JSONObject json) {
         int n_utente = json.getJSONObject("utente").getInt("n_utente");
         String nome = json.getJSONObject("utente").getString("nome");
-        String data_vacina = json.getJSONObject("utente").getString("data_vacina");
+        String email = json.getJSONObject("utente").getString("email");
         String data_nasc = json.getJSONObject("utente").getString("data_nasc");
         String local = json.getJSONObject("utente").getString("local");
         String doencas = json.getJSONObject("utente").getString("doença");
+        String data_vacina = json.getJSONObject("utente").getString("data_vacina");
         List<String> utente_list = new ArrayList<String>();
         utente_list.add(nome);
-        utente_list.add(data_vacina);
+        utente_list.add(email);
         utente_list.add(data_nasc);
         utente_list.add(local);
         utente_list.add(doencas);
+        utente_list.add(data_vacina);
 
         gettingVaccineMap.put(n_utente, utente_list.toString());
     }
--- projService/vaccinationdeskservice/src/main/java/com/vaccinationdesk/vaccinationdeskservice/repository/UtenteRepository.java
+++ projService/vaccinationdeskservice/src/main/java/com/vaccinationdesk/vaccinationdeskservice/repository/UtenteRepository.java
@@ -1,5 +1,10 @@
 package com.vaccinationdesk.vaccinationdeskservice.repository;
 
-public class UtenteRepository {
-    
+import com.vaccinationdesk.vaccinationdeskservice.model.Utente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface UtenteRepository extends JpaRepository<Utente, Integer> {
+    List<Utente> findUtenteByNome(String nome);
+    List<Utente> findUtenteByNumUtente(String nome);
 }
