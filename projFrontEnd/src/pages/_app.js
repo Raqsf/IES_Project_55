19bd5246d68d7fb774a3c3df904fe152d7eba7bd
Raqsf <raquelsf@ua.pt>
2021-12-19 11:41:58 +0000

    Resolve error on _app

--- projFrontEnd/src/pages/_app.js
+++ projFrontEnd/src/pages/_app.js
@@ -6,7 +6,6 @@ import { CssBaseline } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
 import { createEmotionCache } from '../utils/create-emotion-cache';
 import { theme } from '../theme';
-import { BrowserRouter as Router } from "react-router-dom";
 
 const clientSideEmotionCache = createEmotionCache();
 
