857eb27fe56f61d8b1dbfa521923bbe7a443b2a2
TheScorpoi <sobral@ua.pt>
2021-12-20 00:43:09 +0000

    data generated on DG saved on DB

--- projService/vaccinationdeskservice/src/main/java/com/vaccinationdesk/vaccinationdeskservice/broker/MQConsumer.java
+++ projService/vaccinationdeskservice/src/main/java/com/vaccinationdesk/vaccinationdeskservice/broker/MQConsumer.java
@@ -1,23 +1,31 @@
 package com.vaccinationdesk.vaccinationdeskservice.broker;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.sql.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.vaccinationdesk.vaccinationdeskservice.model.Utente;
+import com.vaccinationdesk.vaccinationdeskservice.repository.UtenteRepository;
+
 import org.json.JSONObject;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class MQConsumer {
 
-    private Map<Integer, String> scheduleVaccineMap = new HashMap<Integer, String>();
-    private Map<Integer, String> gettingVaccineMap = new HashMap<Integer, String>();
+    private Map<Integer, Utente> scheduleVaccineMap = new HashMap<Integer, Utente>();
+    private Map<Integer, Utente> gettingVaccineMap = new HashMap<Integer, Utente>();
     private List<Integer> quantityList = new ArrayList<Integer>();
 
+    @Autowired
+    private UtenteRepository utenteRepository;
+
     @RabbitListener(queues = MQConfig.QUEUE)
-    public void listen(String input) {
-        // TODO: Na parte de ter JSONObjects dentro de outros tentar fazer com um iterator (map, i guess)
-        //TODO e fazer isto com as classes, já que estao criadas, pode ser um mapa de Utente for instance
+    public void listen(String input) throws ParseException {
         JSONObject json = new JSONObject(input);
         String messageType = json.getString("type");
         switch (messageType) {
@@ -37,54 +45,50 @@ public class MQConsumer {
         }
     }
 
-    public void addScheduleVaccines(JSONObject json) {
+    public void addScheduleVaccines(JSONObject json) throws ParseException {
         int n_utente = json.getJSONObject("utente").getInt("n_utente");
         String nome = json.getJSONObject("utente").getString("nome");
         String email = json.getJSONObject("utente").getString("email");
         String data_nasc = json.getJSONObject("utente").getString("data_nasc");
         String local = json.getJSONObject("utente").getString("local");
         String doencas = json.getJSONObject("utente").getString("doença");
-        String data_vacina = json.getJSONObject("utente").getString("data_vacina");
-        List<String> utente_list = new ArrayList<String>();
-        utente_list.add(nome);
-        utente_list.add(email);
-        utente_list.add(data_nasc);
-        utente_list.add(local);
-        utente_list.add(doencas);
-        utente_list.add(data_vacina);
-
-        scheduleVaccineMap.put(n_utente, utente_list.toString());
+        String data_agendamento = json.getJSONObject("utente").getString("data_vacina");
+        Date data_nascimento = (Date) new SimpleDateFormat("dd/MM/yy").parse(data_nasc);
+        Date data_agenda = (Date) new SimpleDateFormat("dd/MM/yy").parse(data_agendamento);
+        
+        Utente utente = new Utente(n_utente, nome, email, local, data_nascimento);
+        
+        //para guardar o utente na base de dados
+        utenteRepository.save(utente);
+        
+        scheduleVaccineMap.put(n_utente, utente);
     }
 
-    public void addGettingVaccine(JSONObject json) {
+    public void addGettingVaccine(JSONObject json) throws ParseException {
         int n_utente = json.getJSONObject("utente").getInt("n_utente");
         String nome = json.getJSONObject("utente").getString("nome");
         String email = json.getJSONObject("utente").getString("email");
         String data_nasc = json.getJSONObject("utente").getString("data_nasc");
         String local = json.getJSONObject("utente").getString("local");
         String doencas = json.getJSONObject("utente").getString("doença");
-        String data_vacina = json.getJSONObject("utente").getString("data_vacina");
-        List<String> utente_list = new ArrayList<String>();
-        utente_list.add(nome);
-        utente_list.add(email);
-        utente_list.add(data_nasc);
-        utente_list.add(local);
-        utente_list.add(doencas);
-        utente_list.add(data_vacina);
-
-        gettingVaccineMap.put(n_utente, utente_list.toString());
+        //String data_vacina = json.getJSONObject("utente").getString("data_vacina");
+        Date data_nascimento = (Date) new SimpleDateFormat("dd/MM/yy").parse(data_nasc);  
+        Utente utente = new Utente(n_utente, nome, email, local, data_nascimento);
+        
+        //para guardar o utente na base de dados
+        utenteRepository.save(utente);
+        gettingVaccineMap.put(n_utente, utente);
     }
 
-    public Map<Integer, String> getScheduleVaccineMap() {
+    public Map<Integer, Utente> getScheduleVaccineMap() {
         return scheduleVaccineMap;
     }
 
-    public Map<Integer, String> getGettingVaccineMap() {
+    public Map<Integer, Utente> getGettingVaccineMap() {
         return gettingVaccineMap;
     }
 
     public List<Integer> getQuantityList() {
         return quantityList;
     }
-
 }
